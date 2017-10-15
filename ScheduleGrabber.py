66b5c64081eebeac71f345804774d3fb27f2268f
Sam Hollenbach <shollenb@macalester.edu>
2017-10-15 15:38:56 -0500

    Added throwing info

--- ScheduleGrabber.py
+++ ScheduleGrabber.py
@@ -1,6 +1,7 @@
 import csv
 import datetime
 import requests
+import difflib
 
 def closest_date(dates, today):
     closest = dates[0]
@@ -12,11 +13,10 @@ def closest_date(dates, today):
             closest = d
     return (closest, (today-closest).days)
 
-with open("/home/ec2-user/ThrowingSchedule/ThrowingSchedule.csv", "r") as r:
-    reader = csv.reader(r,delimiter=',')
+def get_pitching_day(data):
     dates = []
     weeks = []
-    for l in reader:
+    for l in data:
         date = l[0]
         date_split = date.split("/")
         if len(date_split) is 3:
@@ -28,12 +28,70 @@ with open("/home/ec2-user/ThrowingSchedule/ThrowingSchedule.csv", "r") as r:
     sunday_day, days_sep = closest_date(dates,today)
     week_index = [w[0] for w in weeks].index(sunday_day)
     current_week = weeks[week_index]
-    current_day = current_week[days_sep+1]
-    headers = {
-        'Content-Type': 'application/json',
-    }
+    return current_week[days_sep+1]
 
-    data = '{{"type": "note", "title": "Throwing Day {}", "body": "{}"}}'.format(today.strftime("%m/%d/%y"), current_day)
+def get_day_info(day, info_file="ThrowingInfo.csv"):
+    data = csv.reader(open(info_file,'r'),delimiter=',')
+    data = [d for d in data]
+    data_fit = []
+
+    data_col1 = []
+    data_col2 = []
+
+    for line in data:
+        line = line[0:4]
+        if line[0] is '':
+            if len(data_col1) > 0:
+                data_fit.append(data_col1)
+                data_col1 = []
+        else:
+            data_col1.append(line)
+    if len(data_col1) > 0:
+        data_fit.append(data_col1)
+
+    for line2 in data:
+        line2 = line2[4:8]
+        if line2[0] is '':
+            if len(data_col2) > 0:
+                data_fit.append(data_col2)
+                data_col2 = []
+        else:
+            data_col2.append(line2)
+    if len(data_col2) > 0:
+            data_fit.append(data_col2)
+
+    names = [d[0][0] for d in data_fit]
+    matches = difflib.get_close_matches(day, names, 1)
+    if len(matches) == 0:
+        return None
+    for d in data_fit:
+        if d[0][0] == matches[0]:
+            return d
 
 
+def push_day(pitching_day, with_info):
+    day_info = get_day_info(pitching_day)
+    info_string = "{}".format(pitching_day)
+    if with_info:
+        info_string += "\\n\\n"
+        for info in day_info[1:]:
+            info = list(filter(lambda a: a != '', info))
+            for i,e in enumerate(info):
+                if e != '':
+                    info_string += "{}".format(e)
+                    if i is not len(info)-1:
+                        info_string += ", "
+            info_string += "\\n"
+    headers = {'Content-Type': 'application/json',}
+    data = '{{"type": "note", "title": "Throwing Day {}", "body": "{}"}}'.format(datetime.date.today().strftime("%m/%d/%y"), info_string)
     requests.post('https://api.pushbullet.com/v2/pushes', headers=headers, data=data, auth=('o.V8x8MqKcPdMqkuot9W3COnIwg5JqPciX', ''))
+
+
+include_info = True
+schedule_file = "ThrowingSchedule.csv"
+
+with open(schedule_file, 'r') as r:
+    data = csv.reader(r,delimiter=',')
+    pitching_day = get_pitching_day(data)
+    push_day(pitching_day, include_info)
+
